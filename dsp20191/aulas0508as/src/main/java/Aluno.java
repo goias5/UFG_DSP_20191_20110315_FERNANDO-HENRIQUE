246bd8c821bb745c1a9020797752a34ac3fe0561
Goias5 <fernandoeq@live.com>
2019-04-03 13:33:31 -0300

    Corrigindo problemas de commits feitos de pcs publicos da ufg

---
+++ dsp20191/aulas0508as/src/main/java/Aluno.java
@@ -0,0 +1,58 @@
+/**
+ * Created by Alunoinf_2 on 21/03/2019.
+ */
+public class Aluno {
+    private String nome;
+    private Long matricula;
+    private double nota1;
+    private double nota2;
+
+    public Aluno(String nome, Long matricula, double nota1, double nota2) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Long matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", matricula=" + matricula +
+                ", nota1=" + nota1 +
+                ", nota2=" + nota2 +
+                '}';
+    }
+}
